fb5c0a885648062535cfc87f374e10a241490988
jackdizhu <376365334@qq.com>
2018-09-01 15:18:33 +0800

    路由修改

--- src/components/public/history.js
+++ src/components/public/history.js
@@ -1,6 +1,11 @@
 /**
  * Created by zengtao on 2017/10/18.
  */
-import createHistory from 'history/createBrowserHistory';
+// import createHistory from 'history/createBrowserHistory';
+import {
+  createBrowserHistory,
+  createHashHistory,
+  createMemoryHistory
+} from 'history'
 
-export default createHistory();
+export default createHashHistory();
